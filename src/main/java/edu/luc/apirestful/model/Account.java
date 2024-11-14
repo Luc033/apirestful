e238da3f2c4ba21a99fd10997a60aa55d5183749
Luc033 <lucasrm33.contato@gmail.com>
2024-11-14 20:47:02 -0300

    add features

---
+++ src/main/java/edu/luc/apirestful/model/Account.java
@@ -0,0 +1,74 @@
+package edu.luc.apirestful.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String agency;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(precision = 13, scale = 2)
+    private BigDecimal balance;
+
+    @Column(name = "additional_limit",precision = 13, scale = 2)
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                "number='" + number + '\'' +
+                ", agency='" + agency + '\'' +
+                ", balance=" + balance +
+                ", limit=" + limit +
+                '}';
+    }
+}
---
+++ src/main/java/edu/luc/apirestful/model/News.java
@@ -0,0 +1,9 @@
+package edu.luc.apirestful.model;
+
+import jakarta.persistence.Entity;
+
+
+@Entity(name="tb_news")
+public class News  extends BaseItem{
+
+}
