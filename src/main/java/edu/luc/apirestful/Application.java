7ed1a21600602c27837c90f0cc2d6ee6ded8abf2
Luc033 <lucasrm33.contato@gmail.com>
2024-11-16 19:04:22 -0300

    add features

--- src/main/java/edu/luc/apirestful/Application.java
+++ src/main/java/edu/luc/apirestful/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApirestfulApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApirestfulApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
